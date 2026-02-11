251c079e084baddecd814482c0396a3e9725874b
rwillisk <rwillisk@gmail.com>
2026-02-11 10:12:22 -0300

    Change the main route

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.redirect("/api");
+});
+
 app.use(router);
 
 app.listen(PORT, () => {
